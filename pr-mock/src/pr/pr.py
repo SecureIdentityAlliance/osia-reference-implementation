20748d8fc3b7b30da3ad2570613ed92c2e644f2a
Olivier Heurtier <olivier.heurtier@secureidentityalliance.org>
2023-05-04 19:28:00 +0200

    Additional services in PR mock

--- pr-mock/src/pr/pr.py
+++ pr-mock/src/pr/pr.py
@@ -1,6 +1,8 @@
 import ssl
 import logging
 import json
+import copy
+import uuid
 
 import aiohttp
 from aiohttp import web
@@ -98,9 +100,32 @@ def serve():
 
 PERSONS = {}
 
+def to_bool(x):
+    if x in [True, 'True', 'true', 1, 'yes', 'y', 'Y', 'Yes', 'YES']:
+        return True
+    return False
+
 # _____________________________________________________________________________
 # PR interface
 # _____________________________________________________________________________
+
+# _____________________________________________________________________________
+@routes.post('/v1/persons')
+@LM.timer("findPersons", ok_status, "error")
+async def findPersons(request):
+    transaction_id = request.query['transactionId']
+    group = to_bool(request.query.get('group', False))
+    reference = to_bool(request.query.get('offset', False))
+    gallery = request.query.get('offset', None)
+    offset = int(request.query.get('offset', 0))
+    limit = int(request.query.get('limit', 100))
+
+    data = await request.json()
+    logging.info("[%s] - findPersons", transaction_id)
+
+    # XXX Not implemented
+    ret = []
+    return web.Response(status=200)
 
 # _____________________________________________________________________________
 @routes.post('/v1/persons/{personId}')
@@ -117,6 +142,85 @@ async def createPerson(request):
     PERSONS[person_id] = data
     PERSONS[person_id]['identities'] = []
     return web.Response(status=201)
+
+# _____________________________________________________________________________
+@LM.timer("readPerson", ok_status, "error")
+async def readPerson(transaction_id, person_id):
+    logging.info("[%s] - readPerson for personId [%s]", transaction_id, person_id)
+    if person_id not in PERSONS:
+        return web.Response(status=404)
+
+    data = copy.copy(PERSONS[person_id])
+    del data['identities']
+    data['personId'] = person_id
+    return web.json_response(data, status=200)
+
+# _____________________________________________________________________________
+@routes.put('/v1/persons/{personId}')
+@LM.timer("updatePerson", ok_status, "error")
+async def updatePerson(request):
+    transaction_id = request.query['transactionId']
+    person_id = request.match_info['personId']
+
+    data = await request.json()
+    logging.info("[%s] - updatePerson for personId [%s]", transaction_id, person_id)
+    if person_id not in PERSONS:
+        return web.Response(status=404)
+
+    data['identities'] = PERSONS[person_id]['identities']
+    PERSONS[person_id] = data
+    return web.Response(status=204)
+
+# _____________________________________________________________________________
+@routes.delete('/v1/persons/{personId}')
+@LM.timer("deletePerson", ok_status, "error")
+async def deletePerson(request):
+    transaction_id = request.query['transactionId']
+    person_id = request.match_info['personId']
+
+    logging.info("[%s] - deletePerson for personId [%s]", transaction_id, person_id)
+    if person_id not in PERSONS:
+        return web.Response(status=404)
+
+    del PERSONS[person_id]
+    return web.Response(status=204)
+
+
+# _____________________________________________________________________________
+@routes.get('/v1/persons/{personId}/identities')
+@LM.timer("readIdentities", ok_status, "error")
+async def readIdentities(request):
+    transaction_id = request.query['transactionId']
+    person_id = request.match_info['personId']
+
+    logging.info("[%s] - readIdentities for personId [%s]", transaction_id, person_id)
+    if person_id not in PERSONS:
+        return web.Response(status=404)
+
+    return web.json_response(PERSONS[person_id]['identities'], status=200)
+
+# _____________________________________________________________________________
+@routes.post('/v1/persons/{personId}/identities')
+@LM.timer("createIdentity", ok_status, "error")
+async def createIdentity(request):
+    transaction_id = request.query['transactionId']
+    person_id = request.match_info['personId']
+    identity_id = uuid.uuid4().hex
+
+    data = await request.json()
+    logging.info("[%s] - createIdentity for personId [%s]", transaction_id, person_id)
+    p = PERSONS.get(person_id, None)
+    if p is None:
+        return web.Response(status=404)
+
+    for i in p['identities']:
+        if i['identityId'] == identity_id:
+            return web.Response(status=409)
+
+    p['identities'].append (data)
+    p['identities'][-1]['identityId'] = identity_id
+
+    return web.json_response({'identityId': identity_id}, status=200)
 
 # _____________________________________________________________________________
 @routes.post('/v1/persons/{personId}/identities/{identityId}')
@@ -139,6 +243,39 @@ async def createIdentityWithId(request):
     p['identities'].append (data)
     p['identities'][-1]['identityId'] = identity_id
     return web.Response(status=201)
+
+# _____________________________________________________________________________
+@routes.get('/v1/persons/{personId}/identities/{identityId}')
+@LM.timer("readIdentity", ok_status, "error")
+async def readIdentity(request):
+    transaction_id = request.query['transactionId']
+    person_id = request.match_info['personId']
+    identity_id = request.match_info['identityId']
+
+    logging.info("[%s] - readIdentity for personId [%s]/[%s]", transaction_id, person_id, identity_id)
+    p = PERSONS.get(person_id, None)
+    if p is None:
+        return web.Response(status=404)
+
+    for i in p['identities']:
+        if i['identityId'] == identity_id:
+            return web.json_response(i, status=200)
+
+    return web.Response(status=404)
+
+# _____________________________________________________________________________
+# Services with the same URL i PR and DataAccess
+# _____________________________________________________________________________
+# _____________________________________________________________________________
+@routes.get('/v1/persons/{id}')
+async def getPersonWithId(request):
+    id = request.match_info['id']
+    names = request.query.getall('attributeNames', [])
+    if names or 'transactionId' not in request.query:
+        return await readPersonAttributes(id, names)
+    transaction_id = request.query['transactionId']
+    return await readPerson(transaction_id, id)
+
 
 # _____________________________________________________________________________
 # Data Access interface
@@ -214,12 +351,8 @@ async def queryPersonList(request):
 
 
 # _____________________________________________________________________________
-@routes.get('/v1/persons/{uin}')
 @LM.timer("readPersonAttributes", ok_status, "error")
-async def readPersonAttributes(request):
-    uin = request.match_info['uin']
-    names = request.query.getall('attributeNames', [])
-
+async def readPersonAttributes(uin, names):
     logging.info("readPersonAttributes for UIN [%s]", uin)
 
     # check person exists and has an identity
--- pr-mock/tests/test_pr.py
+++ pr-mock/tests/test_pr.py
@@ -1,10 +1,10 @@
 import os
 import unittest
 import sys
-import base64
-import hashlib
 
 import requests
+
+import pr.pr
 
 URL = "http://localhost:8080/"
 
@@ -31,17 +31,29 @@ def get_ssl_context():
 
 #_______________________________________________________________________________
 class TestNominal(TestPR):
-    def test_match(self):
+    def setUp(self):
+        pr.pr.PERSONS = {}
+
+    def test_person(self):
+        # Read, person does not exist
+        with requests.get(self.url+'v1/persons/MISS', params={'transactionId': 'T0001'},**get_ssl_context()) as r:
+            assert 404 == r.status_code
+
         # Create person
         data = {
             "status": "ACTIVE",
             "physicalStatus": "DEAD"
         }
         with requests.post(self.url+'v1/persons/0001', json=data, params={'transactionId': 'T0001'},**get_ssl_context()) as r:
+            assert 201 == r.status_code
+
+        # Read
+        with requests.get(self.url+'v1/persons/0001', params={'transactionId': 'T0001'},**get_ssl_context()) as r:
             assert 200 == r.status_code
+            assert {"personId": "0001", "status": "ACTIVE", "physicalStatus": "DEAD"} == r.json()
 
         # Create identity
-        data = {
+        datai = {
             "identityType": "CIVIL",
             "status": "CLAIMED",
             "contextualData": {
@@ -55,234 +67,80 @@ class TestNominal(TestPR):
                 "nationality": "FRA",
             }
         }
-        with requests.post(self.url+'v1/persons/0001/identities/001', json=data, params={'transactionId': 'T0001'},**get_ssl_context()) as r:
+        with requests.post(self.url+'v1/persons/0001/identities/001', json=datai, params={'transactionId': 'T0001'},**get_ssl_context()) as r:
+            assert 201 == r.status_code
+
+        # Read, make sure no identity is returned
+        with requests.get(self.url+'v1/persons/0001', params={'transactionId': 'T0001'},**get_ssl_context()) as r:
             assert 200 == r.status_code
+            assert {"personId": "0001", "status": "ACTIVE", "physicalStatus": "DEAD"} == r.json()
 
-        # Tests some matchings
-        data = {
-            "firstName": "Jane",
-            "lastName": "Doe",
-        }
-        with requests.post(self.url+'v1/persons/0001/match', json=data,**get_ssl_context()) as r:
-            assert 200 == r.status_code
-            assert [] == r.json()
-
-        data = {
-            "firstName": "Jane",
-            "lastName": "Doe2",
-            "noname": "blabla,"
-        }
-        with requests.post(self.url+'v1/persons/0001/match', json=data, **get_ssl_context()) as r:
-            assert 200 == r.status_code
-            assert [{'attributeName': 'lastName', 'errorCode': 1}, {'attributeName': 'noname', 'errorCode': 0}] == r.json()
-
-    def test_query(self):
-        # Create persons
-        for n in range(10):
-            data = {
-                "status": "ACTIVE",
-                "physicalStatus": "DEAD"
-            }
-            with requests.post(self.url+f'v1/persons/{n}', json=data, params={'transactionId': 'T0002'},**get_ssl_context()) as r:
-                assert 200 == r.status_code
-
-            # Create identity
-            data = {
-                "identityType": "CIVIL",
-                "status": "CLAIMED",
-                "contextualData": {
-                    "enrollmentDate": "2019-01-11",
-                },
-                "biographicData": {
-                    "firstName": "John",
-                    "lastName": f"Doe{n}",
-                    "dateOfBirth": "1985-11-30",
-                    "gender": "M",
-                    "nationality": "FRA",
-                }
-            }
-            with requests.post(self.url+f'v1/persons/{n}/identities/001', json=data, params={'transactionId': 'T0002'},**get_ssl_context()) as r:
-                assert 200 == r.status_code
-
-        # Tests some queries
-        # 1 result
-        params = {
-            "firstName": "John",
-            "lastName": "Doe1",
-        }
-        with requests.get(self.url+'v1/persons', params=params,**get_ssl_context()) as r:
-            assert 200 == r.status_code
-            assert ['1'] == r.json()
-
-        # no result
-        params = {
-            "firstName": "Jane",
-            "lastName": "Doe1",
-        }
-        with requests.get(self.url+'v1/persons', params=params,**get_ssl_context()) as r:
-            assert 200 == r.status_code
-            assert [] == r.json()
-
-        # large number of result
-        params = {
-            "firstName": "John",
-        }
-        with requests.get(self.url+'v1/persons', params=params,**get_ssl_context()) as r:
-            assert 200 == r.status_code
-            assert ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'] == r.json()
-
-        # one result + attributes
-        params = {
-            "firstName": "John",
-            "lastName": "Doe1",
-            "names":['lastName', 'gender']
-        }
-        with requests.get(self.url+'v1/persons', params=params,**get_ssl_context()) as r:
-            assert 200 == r.status_code
-            assert [{'lastName': 'Doe1', 'gender': 'M'}] == r.json()
-
-        # large number of result with offset and limit
-        params = {
-            "firstName": "John",
-            "offset": 2,
-            "limit": 3,
-        }
-        with requests.get(self.url+'v1/persons', params=params,**get_ssl_context()) as r:
-            assert 200 == r.status_code
-            assert ['2', '3', '4'] == r.json()
-
-    def test_read(self):
-        # Create person
+        # Update
         data = {
             "status": "ACTIVE",
             "physicalStatus": "ALIVE"
         }
-        with requests.post(self.url+'v1/persons/0002', json=data, params={'transactionId': 'T0001'},**get_ssl_context()) as r:
+        with requests.put(self.url+'v1/persons/0001', json=data, params={'transactionId': 'T0001'},**get_ssl_context()) as r:
+            assert 204 == r.status_code
+
+        # Read
+        with requests.get(self.url+'v1/persons/0001', params={'transactionId': 'T0001'},**get_ssl_context()) as r:
             assert 200 == r.status_code
+            assert {"personId": "0001", "status": "ACTIVE", "physicalStatus": "ALIVE"} == r.json()
+
+        # delete
+        with requests.delete(self.url+'v1/persons/0001', params={'transactionId': 'T0001'},**get_ssl_context()) as r:
+            assert 204 == r.status_code
+
+        # Read, person does not exist
+        with requests.get(self.url+'v1/persons/MISS', params={'transactionId': 'T0001'},**get_ssl_context()) as r:
+            assert 404 == r.status_code
+
+    def test_identity(self):
+        # Create person
+        data = {
+            "status": "ACTIVE",
+            "physicalStatus": "DEAD"
+        }
+        with requests.post(self.url+'v1/persons/0001', json=data, params={'transactionId': 'T0001'},**get_ssl_context()) as r:
+            assert 201 == r.status_code
 
         # Create identity
-        data = {
+        datai = {
             "identityType": "CIVIL",
             "status": "CLAIMED",
             "contextualData": {
                 "enrollmentDate": "2019-01-11",
             },
             "biographicData": {
-                "firstName": "Bob",
+                "firstName": "Jane",
                 "lastName": "Doe",
                 "dateOfBirth": "1985-11-30",
                 "gender": "F",
                 "nationality": "FRA",
             }
         }
-        with requests.post(self.url+'v1/persons/0002/identities/001', json=data, params={'transactionId': 'T0001'},**get_ssl_context()) as r:
+        with requests.post(self.url+'v1/persons/0001/identities/001', json=datai, params={'transactionId': 'T0001'},**get_ssl_context()) as r:
+            assert 201 == r.status_code
+
+        datai["contextualData"]["enrollmentDate"] = "2020-01-11"
+        with requests.post(self.url+'v1/persons/0001/identities', json=datai, params={'transactionId': 'T0001'},**get_ssl_context()) as r:
+            assert 200 == r.status_code
+            assert r.json()['identityId'] != ''
+
+        # Read all identities
+        with requests.get(self.url+'v1/persons/0001/identities', params={'transactionId': 'T0001'},**get_ssl_context()) as r:
+            assert 200 == r.status_code
+            assert 2 == len(r.json())
+            assert r.json()[0]["contextualData"]["enrollmentDate"] != r.json()[1]["contextualData"]["enrollmentDate"]
+
+        # Read 1 identity
+        with requests.get(self.url+'v1/persons/0001/identities/001', params={'transactionId': 'T0001'},**get_ssl_context()) as r:
             assert 200 == r.status_code
 
-        # Tests some reads
-        params = {
-            "attributeNames": ["firstName", "lastName"]
-        }
-        with requests.get(self.url+'v1/persons/0002', params=params,**get_ssl_context()) as r:
-            assert 200 == r.status_code
-            assert {"firstName": "Bob", "lastName": "Doe",} == r.json()
-
-    def test_birth(self):
-        # Create 2 persons
-        data = {
-            "status": "ACTIVE",
-            "physicalStatus": "ALIVE"
-        }
-        with requests.post(self.url+'v1/persons/A', json=data, params={'transactionId': 'birth'},**get_ssl_context()) as r:
-            assert 200 == r.status_code
-
-        # Create identity
-        data = {
-            "identityType": "CIVIL",
-            "status": "VALID",
-            "contextualData": {
-                "enrollmentDate": "2019-01-11",
-            },
-            "biographicData": {
-                "firstName": "Albert",
-                "lastName": "Smith",
-                "dateOfBirth": "1985-11-30",
-                "gender": "M",
-                "nationality": "FRA",
-            }
-        }
-        with requests.post(self.url+'v1/persons/A/identities/001', json=data, params={'transactionId': 'birth'},**get_ssl_context()) as r:
-            assert 200 == r.status_code
-
-        data = {
-            "status": "ACTIVE",
-            "physicalStatus": "ALIVE"
-        }
-        with requests.post(self.url+'v1/persons/B', json=data, params={'transactionId': 'birth'},**get_ssl_context()) as r:
-            assert 200 == r.status_code
-
-        # Create identity
-        data = {
-            "identityType": "CIVIL",
-            "status": "VALID",
-            "contextualData": {
-                "enrollmentDate": "2019-01-11",
-            },
-            "biographicData": {
-                "firstName": "Alice",
-                "lastName": "Smith",
-                "dateOfBirth": "1987-11-30",
-                "gender": "F",
-                "nationality": "FRA",
-            }
-        }
-        with requests.post(self.url+'v1/persons/B/identities/001', json=data, params={'transactionId': 'birth'},**get_ssl_context()) as r:
-            assert 200 == r.status_code
-
-
-        # match mother
-        data = {
-            "firstName": "Alice",
-            "lastName": "Smith",
-        }
-        with requests.post(self.url+'v1/persons/B/match', json=data,**get_ssl_context()) as r:
-            assert 200 == r.status_code
-            assert [] == r.json()
-
-        # match father
-        data = {
-            "firstName": "Albert",
-            "lastName": "Smith",
-        }
-        with requests.post(self.url+'v1/persons/A/match', json=data,**get_ssl_context()) as r:
-            assert 200 == r.status_code
-            assert [] == r.json()
-
-        # read mother attributes
-        params = {
-            "attributeNames": ["dateOfBirth"]
-        }
-        with requests.get(self.url+'v1/persons/B', params=params,**get_ssl_context()) as r:
-            assert 200 == r.status_code
-            assert {"dateOfBirth": "1987-11-30"} == r.json()
-
-        # read father attributes
-        params = {
-            "attributeNames": ["dateOfBirth"]
-        }
-        with requests.get(self.url+'v1/persons/A', params=params,**get_ssl_context()) as r:
-            assert 200 == r.status_code
-            assert {"dateOfBirth": "1985-11-30"} == r.json()
-
-        # check if new born exists
-        params = {
-            "firstName": "Baby",
-            "lastName": "Smith",
-            "dateOfBirth": "2023-04-15"
-        }
-        with requests.get(self.url+'v1/persons', params=params,**get_ssl_context()) as r:
-            assert 200 == r.status_code
-            assert [] == r.json()
-
+        # Read missing identity
+        with requests.get(self.url+'v1/persons/0001/identities/MISS', params={'transactionId': 'T0001'},**get_ssl_context()) as r:
+            assert 404 == r.status_code
 
 if __name__ == '__main__':
     unittest.main(argv=['-v'])
